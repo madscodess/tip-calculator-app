0b51759165f3919ac0114371ecd45d4544cc1cdc
madscodess <32851804+madscodess@users.noreply.github.com>
2024-03-29 13:38:27 +0000

    JS updated for to display tip amout and total amount per person

--- app.js
+++ app.js
@@ -5,42 +5,68 @@ console.log(billTotal.value);
 const buttons = document.querySelectorAll('button');
 //get custom tip input
 let customTip = document.getElementById('tip-custom');
+//get reset button
+resetButton = document.getElementById('reset');
+//set tipAmount
+let tipAmount; 
+//text
+let tipPerPersonText = document.getElementById('tipAmountPerPerson');
+let totalPerPersonText = document.getElementById('totalAmountPerPerson');
 
 // Add a eventListener to every button
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function() { // when a button gets clicked, an function will fire
     console.log("button clicked");
     //button value (5, 10 etc / 100 * the value of the bill inputted)
-    let tipAmount = parseFloat(this.value / 100 * billTotal.value);
+    tipAmount = parseFloat(this.value / 100 * billTotal.value);
    console.log(tipAmount);
   // console.log(customTip.value);
   });
+}
 
-  customTip.oninput = function(){
+  // Event listener for custom tip input
+customTip.oninput = function() {
   console.log(this.value);
-  let tipAmountCustom = parseFloat(this.value / 100 * billTotal.value);
- //console.log(tipAmountCustom);
+  // Calculate tipAmount based on custom tip input
+  tipAmount = parseFloat(this.value / 100 * billTotal.value);
+  console.log(tipAmount);
+  // Recalculate tip per person and update the display
+  calculateTipPerPerson();
 }; 
 
+// Function to calculate tip per person and update display
+function calculateTipPerPerson() {
+  // Get number of people input
+  let numberOfPeopleAmount = parseFloat(document.getElementById('people').value);
+  // Calculate tip per person
+  let tipPerPerson = parseFloat(tipAmount / numberOfPeopleAmount);
+  // Calculate bill per person
+  let billPerPerson = parseFloat(billTotal.value / numberOfPeopleAmount);
+  // Update tip per person text
+  tipPerPersonText.innerHTML = tipPerPerson.toFixed(2);
+  // Calculate total amount per person (bill + tip)
+  let totalAmountPerPerson = billPerPerson + tipPerPerson;
+  // Update total amount per person text
+  totalPerPersonText.innerHTML = totalAmountPerPerson.toFixed(2); 
 }
 
-//get number of people input & make new variables with input
-let numberOfPeople = document.getElementById('people').oninput = function(){
-  //console.log(this.value / customTip.value);
- let numberOfPeopleAmount = this;
-  //let tipPerPerson = parseFloat(tipAmount.value / this.value);
-  let tipPerPersonCustom = parseFloat(customTip.value / numberOfPeopleAmount.value);
-  let billPerPerson = parseFloat(billTotal.value / this.value);
-  // let totalPerPerson = parseFloat(tipPerPerson plus );
- //console.log(tipPerPerson);
-  //console.log(tipPerPersonCustom);
-  console.log(billPerPerson);
-}
+// Event listener for number of people input
+document.getElementById('people').oninput = calculateTipPerPerson;
 
+//reset button
+resetButton.addEventListener('click', function() {
+    // Clear the bill total input value
+    billTotal.value = '';
+    
+    // Clear the custom tip input value
+    customTip.value = '';
 
-// let customTip = document.getElementById('tip-custom');
-// if customTip != 0?? 
+    // Clear the number of people input value
+    let numberOfPeople = document.getElementById('people');
+    numberOfPeople.value = '';
 
-
-
+    // Clear displayed tip per person and total amount per person
+    tipPerPersonText.innerHTML = '';
+    totalPerPersonText.innerHTML = '';
+});
 
